5016591e00249733c09fb27bacd0e89580cb66ce
Bart Jeukendrup <bart@jeukendrup.nl>
2017-11-26 18:31:56 +0200

    added ability to configure walled_garden page

--- start.php
+++ start.php
@@ -5,6 +5,8 @@ function html_pages_init() {
     elgg_register_event_handler('pagesetup', 'system', 'html_pages_pagesetup');
     elgg_register_action('html_pages/settings', dirname(__FILE__) . '/actions/html_pages/settings.php', 'admin');
 
+    elgg_register_plugin_hook_handler('index', 'system', 'html_pages_index_handler', 0); // make sure priority is zero to be before walled garden
+
     elgg_extend_view('js/admin', 'html_pages/js/admin');
     elgg_extend_view('css/admin', 'html_pages/css/admin');
 
@@ -35,11 +37,11 @@ function html_pages_get_page($handler) {
 }
 
 function html_pages_register_page($page) {
-    if ($page['url'] === "index") {
-        elgg_register_plugin_hook_handler('index', 'system', 'html_pages_index_handler', 0); // make sure priority is zero to be before walled garden
-    } else {
-        elgg_register_page_handler($page['url'], 'html_pages_page_handler');
+    if ($page['url'] === "index" || $page['url'] === "walled_garden") {
+        return;
     }
+
+    elgg_register_page_handler($page['url'], 'html_pages_page_handler');
 }
 
 function html_pages_pagesetup() {
@@ -47,10 +49,18 @@ function html_pages_pagesetup() {
 }
 
 function html_pages_index_handler($hook, $type, $return_value, $params) {
-    $page = html_pages_get_page("index");
+    $logged_in = elgg_is_logged_in();
 
-    if ($page) {
-        echo elgg_view_page(null, $page['content'], "default");
+    $index = html_pages_get_page("index");
+    $walled_garden = html_pages_get_page("walled_garden");
+
+    if (!$logged_in && $walled_garden) {
+        echo elgg_view_page(null, $walled_garden['content'], "default");
+        return true;
+    }
+
+    if ($logged_in && $index) {
+        echo elgg_view_page(null, $index['content'], "default");
         return true;
     }
 }
